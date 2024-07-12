c57b17bfea739603946aa8fbbbe415d8d85d6917
Bozhidar Toshev <bozidartosev88@gmail.com>
2024-07-12 23:33:08 +0300

    made the ham-menu to make overflow-y hidden

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const hamMenu = document.querySelector(".ham-menu");
-
 const offScreenMenu = document.querySelector(".off-screen-menu");
+const overlay = document.querySelector('.overlay');
+const body = document.body;
 
 hamMenu.addEventListener("click", () => {
   hamMenu.classList.toggle("active");
   offScreenMenu.classList.toggle("active");
-  document.querySelector('.overlay').style.display = document.querySelector('.off-screen-menu').classList.contains('active') ? 'block' : 'none';
+  overlay.style.display = offScreenMenu.classList.contains('active') ? 'block' : 'none';
+  body.style.overflowY = offScreenMenu.classList.contains("active") ? "hidden" : "auto";
 });
