17d6cea9441dbb629ad5804eede1f7f0c623191a
Bozhidar Toshev <bozidartosev88@gmail.com>
2024-07-13 23:53:59 +0300

    removed scroll bar

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const hamMenu = document.querySelector(".ham-menu");
 const offScreenMenu = document.querySelector(".off-screen-menu");
 const overlay = document.querySelector('.overlay');
-const body = document.body;
 
 hamMenu.addEventListener("click", () => {
   hamMenu.classList.toggle("active");
   offScreenMenu.classList.toggle("active");
   overlay.style.display = offScreenMenu.classList.contains('active') ? 'block' : 'none';
-  body.style.overflowY = offScreenMenu.classList.contains("active") ? "hidden" : "auto";
 });
